5cdc63c5d4caf6975463662e0c83c7425526df86
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-19 11:31:34 +0300

    Divvirzienu saistīts rezultātu palielinātjs

--- src/Counter.js
+++ src/Counter.js
@@ -2,16 +2,36 @@ import { useState } from "react";
 
 function Counter() {
   const [result, setResult] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
 
   function addOne() {
     console.log("Funkcija izsaukta");
     setResult(result + 1);
-    console.log("x vērtība ir " + result);
+    console.log("result vērtība ir " + result);
+  }
+
+  // Reizināt rezultātu ar
+  // lietotāja ievadītu skaitli
+
+  function minusOne() {
+    setResult(result - 1);
+  }
+
+  // Uztaisi pogu, kas izsauvc reizināšanu
+  function multiply() {
+    setResult(result * multiplier);
+  }
+
+  function handleChange(kaķēns) {
+    setMultiplier(kaķēns.target.value);
   }
 
   return (
     <div>
       <button onClick={addOne}>+ 1</button>
+      <button onClick={minusOne}>- 1</button>
+      <input type="number" value={multiplier} onChange={handleChange} />
+      <button onClick={multiply}>* {multiplier}</button>
       <h1>{result}</h1>
     </div>
   );
